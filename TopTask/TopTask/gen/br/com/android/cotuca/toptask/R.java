f9326305c76144fa86df31ce2735defcfbb286fa
TopTask <toptask.tcc@gmail.com>
2014-09-04 11:40:44 -0300

    mudando icone

--- TopTask/TopTask/gen/br/com/android/cotuca/toptask/R.java
+++ TopTask/TopTask/gen/br/com/android/cotuca/toptask/R.java
@@ -36,18 +36,18 @@ public final class R {
     }
     public static final class drawable {
         public static final int ab_solid_toptask_theme=0x7f020000;
-        public static final int drawer_shadow=0x7f020001;
-        public static final int fundo_entrada=0x7f020002;
-        public static final int ic_action_accept=0x7f020003;
-        public static final int ic_action_discard=0x7f020004;
-        public static final int ic_action_edit=0x7f020005;
-        public static final int ic_action_good=0x7f020006;
-        public static final int ic_action_new=0x7f020007;
-        public static final int ic_action_overflow=0x7f020008;
-        public static final int ic_drawer=0x7f020009;
-        public static final int ic_launcher=0x7f02000a;
-        public static final int icon=0x7f02000b;
-        public static final int icon1=0x7f02000c;
+        public static final int b_icon=0x7f020001;
+        public static final int drawer_shadow=0x7f020002;
+        public static final int fundo_entrada=0x7f020003;
+        public static final int ic_action_accept=0x7f020004;
+        public static final int ic_action_discard=0x7f020005;
+        public static final int ic_action_edit=0x7f020006;
+        public static final int ic_action_good=0x7f020007;
+        public static final int ic_action_new=0x7f020008;
+        public static final int ic_action_overflow=0x7f020009;
+        public static final int ic_drawer=0x7f02000a;
+        public static final int ic_launcher=0x7f02000b;
+        public static final int icon=0x7f02000c;
         public static final int new_task=0x7f02000d;
     }
     public static final class id {
