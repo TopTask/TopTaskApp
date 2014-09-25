de05d9c8a7d283a1adbcb0cd57eb492abaf38b84
unknown <u12161@LAPA24.academico.cotuca.unicamp.br>
2014-09-25 20:27:15 -0300

    Implementação do click do adapter. Qual o id da tarefa?

--- TopTask/TopTask/src/br/com/android/cotuca/toptask/Activitys/QuadroTarefaActivity.java
+++ TopTask/TopTask/src/br/com/android/cotuca/toptask/Activitys/QuadroTarefaActivity.java
@@ -1,20 +1,21 @@
 package br.com.android.cotuca.toptask.Activitys;
 
-import java.util.List;
-
-import br.com.android.cotuca.toptask.R;
-import br.com.android.cotuca.toptask.BD.ContratoProjetos;
-import br.com.android.cotuca.toptask.Beans.Tarefa;
-import br.com.android.cotuca.toptask.DAO.TarefaDAO;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.PopupMenu;
+import android.widget.PopupMenu.OnMenuItemClickListener;
+import br.com.android.cotuca.toptask.R;
+import br.com.android.cotuca.toptask.DAO.TarefaDAO;
 
-public class QuadroTarefaActivity extends Activity{
+public class QuadroTarefaActivity extends Activity implements OnMenuItemClickListener{
 
 	private Bundle dadosRecebidos;
+	private int idMover;
 	
 	@Override
 	protected void onCreate(Bundle estado) {
@@ -22,28 +23,31 @@ public class QuadroTarefaActivity extends Activity{
 		super.onCreate(estado);
 		setContentView(R.layout.activity_quadro_tarefas);
 		dadosRecebidos = getIntent().getExtras();
-		
-		selecionaFragmentAdequado(dadosRecebidos);
-	}
-
-	private void selecionaFragmentAdequado(Bundle dados) {
-		TarefaDAO tarefas = TarefaDAO.getInstance(this);
-		int idProjeto = dados.getInt(ContratoProjetos.Colunas._ID);
-		
-		List<Tarefa> tarefasProjeto = tarefas.getTarefasProjetos(idProjeto);
-		
-		if (tarefasProjeto.isEmpty()){ // Carregar o FragmentQuadroTarefasVazio
-		    
-		}else{
-			
-			
-		}
 	}
 	
 	public void showPopup(View v){
 		PopupMenu pop = new PopupMenu(this, v);
 		MenuInflater inflater = pop.getMenuInflater();
-		inflater.inflate(R.menu.);
+		inflater.inflate(R.menu.mover_tarefa, pop.getMenu());
+
+		Log.i("Efeito do tarefas", String.valueOf(v.getId()));
+		pop.show();
+	}
+	
+	@Override
+	public boolean onMenuItemClick(MenuItem item) {
+		TarefaDAO tarefas = TarefaDAO.getInstance(this);
+		switch(item.getItemId()){
+			case R.id.mover_fazer:
+
+			case R.id.mover_fazendo:
+
+			case R.id.mover_feito:
+
+		}
+		
+		this.recreate();
+		return false;
 	}
 	
 }
